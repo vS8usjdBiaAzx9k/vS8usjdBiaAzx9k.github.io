adb74c5406b182cd6a8fe878170e8db08de50477
smoothbronx <vlad.uhwatov@yandex.ru>
2022-12-01 00:33:41 +0200

    Card added

---
+++ card/static/js/particle.js
@@ -0,0 +1,62 @@
+particlesJS("main", {
+    "particles": {
+        "number": {
+            "value": 600,
+            "density": {
+                "enable": true,
+                "value_area": 789.1476416322727
+            }
+        },
+        "color": {
+            "value": "#ffc400"
+        },
+        "opacity": {
+            "value": 0.48927153781200905,
+            "random": false,
+            "anim": {
+                "enable": true,
+                "speed": 0.2,
+                "opacity_min": 0,
+                "sync": false
+            }
+        },
+        "size": {
+            "value": 1,
+            "random": true,
+            "anim": {
+                "enable": true,
+                "speed": 2,
+                "size_min": 0,
+                "sync": false
+            }
+        },
+        "line_linked": {
+            "enable": false,
+            "distance": 150,
+            "color": "#ffffff",
+            "opacity": 0.4,
+            "width": 1
+        },
+        "move": {
+            "enable": true,
+            "speed": 0.2,
+            "direction": "none",
+            "random": true,
+            "straight": false,
+            "out_mode": "out",
+            "bounce": false,
+            "attract": {
+                "enable": false,
+                "rotateX": 600,
+                "rotateY": 1200
+            }
+        }
+    },
+    "interactivity": {
+        "detect_on": "canvas",
+        "events": {
+            "resize": true
+        },
+    },
+    "retina_detect": true
+});
